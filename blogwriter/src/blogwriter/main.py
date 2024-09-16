a4da9b5fe75bb0f0a4e49e225aadcb4c76e4fb46
tgt <kasyap3103@gmail.com>
2024-09-16 15:20:41 +0530

    first version

--- blogwriter/src/blogwriter/main.py
+++ blogwriter/src/blogwriter/main.py
@@ -2,23 +2,30 @@ from crewai import  Crew, Process
 from crewai.project import  crew
 from blog_tasks import BlogTasks
 from blog_agents import BlogAgents
+from langchain_ollama import ChatOllama
+import os
 
+os.environ["OPENAI_API_KEY"] = "NA"
 
-editor=BlogAgents.editor()
-planner=BlogAgents.planner()
-writer=BlogAgents.writer()
-writeTask = BlogTasks.write(writer)
-editTask = BlogTasks.edit(editor)
-planTask = BlogTasks.plan(planner)
+llm = ChatOllama(model="gemma2:2b", base_url="http://localhost:11434")
+blog_agents=BlogAgents()
+blog_tasks=BlogTasks()
+editor=blog_agents.editor(llm)
+planner=blog_agents.planner(llm)
+writer=blog_agents.writer(llm)
+writeTask = blog_tasks.write(writer)
+editTask = blog_tasks.edit(editor)
+planTask = blog_tasks.plan(planner)
 
 
 BlogCrew=Crew(
-    agents=[writer,planner,editor],
-    tasks=[editTask,writeTask,planTask],
+    agents=[planner,writer,editor],
+    tasks=[planTask,editTask,writeTask],
     verbose=True,
     process=Process.sequential
 )
 
 input={"topic": "SSH , SSH tunneling, portForwarding and use case of NGRock"}
-result=BlogCrew.kickoff(input=input)
+result=BlogCrew.kickoff(inputs=input)
+
 print(result); 
