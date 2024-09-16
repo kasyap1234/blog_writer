9e02fd958805a81e9e8bec6d0dbd33159ac44e35
tgt <kasyap3103@gmail.com>
2024-09-16 15:26:28 +0530

    prompt modify

--- blogwriter/src/blogwriter/blog_agents.py
+++ blogwriter/src/blogwriter/blog_agents.py
@@ -15,7 +15,7 @@ class BlogAgents():
     def writer(self,llm) -> Agent:
         return Agent(
 			role=f"Content Writer",
-			goal="Write insightful and factually accurate opinion piece about the topic: {topic}",
+			goal="Write insightful and factually accurate and detailed opinion piece about the topic: {topic}",
 			backstory="You're working on writing a new opinion piece about the topic: {topic} '...",
 			allow_delegation=False,
 			llm=llm,
@@ -25,7 +25,7 @@ class BlogAgents():
     def editor(self, llm):
         return Agent(
             role="Editor",
-            goal="Edit a given blog post to align with the writing style ,needs to be factually correct and technical explanation needs to be explained with real life examples ",
+            goal="Edit a given blog post to align with the writing style and the target audience is software developers  ,needs to be factually correct and technical explanation needs to be explained with real life examples ",
             backstory="You are an editor who receives a blog post from the Content Writer...",
             llm=llm,
             allow_delegation=False,
--- blogwriter/src/blogwriter/blog_tasks.py
+++ blogwriter/src/blogwriter/blog_tasks.py
@@ -14,14 +14,14 @@ class BlogTasks():
    
     def write(self,agent):
         return Task(
-            description="1. Use the content plan to craft a compelling blog post on {topic}...",
-            expected_output="A well-written blog post in markdown format, ready for publication, each section should have 2 or 3 paragraphs.",
+            description="1. Use the content plan to craft a well detailed,technical and compelling blog post on {topic}, the {topic} should be explained in a detailed manner with real life examples . ",
+            expected_output="A well-written detailed blog post in markdown format, ready for publication, each section should have 2 or 3 paragraphs.",
             agent=agent
         )
     
     def edit(self,agent):
         return Task(
-            description="Proofread the given blog post for grammatical errors and alignment with the brand's voice.",
-            expected_output="A well-written blog post in markdown format, ready for publication, each section should have 2 or 3 paragraphs.",
+            description="Proofread the given blog post for grammatical errors and alignment for developers . ",
+            expected_output="A well-written detailed blog post in markdown format, ready for publication, each section should have 2 or 3 paragraphs.",
             agent=agent 
         )
