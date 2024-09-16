fc6c38b8aec88e0f620eccd63ec91fd8fff89843
tgt <kasyap3103@gmail.com>
2024-09-16 13:26:30 +0530

    updated code

--- blogwriter/src/blogwriter/blog_tasks.py
+++ blogwriter/src/blogwriter/blog_tasks.py
@@ -1,28 +1,27 @@
 from crewai import Task
-from crewai import agents
-from agents import Agents 
 
-class BlogTasks:
-    def __init__(self, agents):
-        self.agents = agents
-    @Task
-    def plan(self):
+
+
+class BlogTasks():
+    
+  
+    def plan(self,agent):
         return Task(
             description="1. Prioritize the latest trends, key players, and noteworthy news on {topic}...",
             expected_output="A comprehensive content plan document with an outline, audience analysis, SEO keywords, and resources.",
-            agent=self.agents.planner(),
+            agent=agent 
         )
-    @Task
-    def write(self):
+   
+    def write(self,agent):
         return Task(
             description="1. Use the content plan to craft a compelling blog post on {topic}...",
             expected_output="A well-written blog post in markdown format, ready for publication, each section should have 2 or 3 paragraphs.",
-            agent=self.agents.writer(),
+            agent=agent
         )
-    @Task 
-    def edit(self):
+    
+    def edit(self,agent):
         return Task(
             description="Proofread the given blog post for grammatical errors and alignment with the brand's voice.",
             expected_output="A well-written blog post in markdown format, ready for publication, each section should have 2 or 3 paragraphs.",
-            agent=self.agents.editor(),
+            agent=agent 
         )
--- blogwriter/src/blogwriter/main.py
+++ blogwriter/src/blogwriter/main.py
@@ -1,73 +1,24 @@
-#!/usr/bin/env python
-import sys
-from crew import BlogwriterCrew
-from crew import BlogwriterCrew
-from langchain_ollama import ChatOllama
-
-# This main file is intended to be a way for your to run your
-# crew locally, so refrain from adding necessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
-
-def run():
-    """
-    Run the crew.
-    """
-    inputs = {
-        'topic': 'AI LLMs'
-    }
-    BlogwriterCrew().crew().kickoff(inputs=inputs)
+from crewai import  Crew, Process
+from crewai.project import  crew
+from blog_tasks import BlogTasks
+from blog_agents import BlogAgents
 
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        BlogwriterCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        BlogwriterCrew().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        BlogwriterCrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
+editor=BlogAgents.editor()
+planner=BlogAgents.planner()
+writer=BlogAgents.writer()
+writeTask = BlogTasks.write(writer)
+editTask = BlogTasks.edit(editor)
+planTask = BlogTasks.plan(planner)
 
 
-# Initialize the LLM
-llm = ChatOllama(model="gemma2", base_url="http://localhost:11434")
+BlogCrew=Crew(
+    agents=[writer,planner,editor],
+    tasks=[editTask,writeTask,planTask],
+    verbose=True,
+    process=Process.sequential
+)
 
-# Create an instance of BlogwriterCrew
-blogwriter_crew = BlogwriterCrew(llm=llm)
-
-# Get the crew
-crew = blogwriter_crew.crew()
-
-# Define your inputs
-inputs = {"topic": "SSH, port forwarding and use of NGrock"}
-
-# Kickoff the crew
-result = crew.kickoff(inputs=inputs)
-print(result)
+input={"topic": "SSH , SSH tunneling, portForwarding and use case of NGRock"}
+result=BlogCrew.kickoff(input=input)
+print(result); 
